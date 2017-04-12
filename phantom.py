6910922b23225548a22d981a26fce7f5623f623c
Jordan Ott <jordanott365@gmail.com>
2017-04-11 23:34:20 -0700

    Add files via upload

--- phantom.py
+++ phantom.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.by import By
 # https://www.mathworks.com/matlabcentral/fileexchange/?page=1&term=type%3AFunction
 matlab_url = "https://www.mathworks.com/matlabcentral/fileexchange/?page={0}&term=type%3AFunction"
 login = "https://www.mathworks.com/login?uri=https%3A%2F%2Fwww.mathworks.com%2Fmatlabcentral%2Ffileexchange%2F%3Fterm%3Dtype%253AFunction&form_type=community"
-dl_dir = '/path/to/downloads/dir/'
+dl_dir = '/home/jordan/Documents/Matlab/Matlab_Downloads/'
 
 display = Display(visible=0, size=(800, 600))
 display.start()
@@ -20,7 +20,7 @@ dl_location = os.path.join(os.getcwd(), dl_dir)
 
 prefs = {"download.default_directory": dl_location}
 chrome_options.add_experimental_option("prefs", prefs)
-chromedriver = "/path/to/chromedriver"
+chromedriver = "/home/jordan/Documents/Matlab/Selenium_Drivers/chromedriver"
 driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=chrome_options)
 
 driver.set_window_size(800, 600)
@@ -32,20 +32,17 @@ driver.switch_to.frame(driver.find_element_by_tag_name("iframe"))
 time.sleep(10)
 
 # enter login credentials
-driver.find_element_by_id('userId').send_keys('**USER NAME**')
-driver.find_element_by_id('password').send_keys('**PASSWORD**')
+driver.find_element_by_id('userId').send_keys('jordanott365@gmail.com')
+driver.find_element_by_id('password').send_keys('Jordan1@')
 driver.find_element_by_id('submit').click()
 time.sleep(10)
 
 # switch back to html default
 driver.switch_to.default_content()
 
-# check if we are at the last page of links
-def last_page():
-	return False
-
-page_num = 1
-while not last_page():
+TOTAL_PAGES = 446
+current_page = 1
+while current_page < TOTAL_PAGES:
 	index = 49
 	while index < 50:
 		table_body = driver.find_elements_by_tag_name('tbody')
@@ -69,10 +66,9 @@ while not last_page():
 		driver.back()
 		time.sleep(4)
 		index += 1
-	page_num += 1
-	driver.get(matlab_url.format(page_num))
+	current_page += 1
+	driver.get(matlab_url.format(current_page))
 	
-
 
 driver.close()
 display.stop()
